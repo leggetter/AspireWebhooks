6e8597a4af6367d955271836503912848a4a3ac2
Phil Leggetter <phil@leggetter.co.uk>
2024-09-18 11:27:53 +0100

    chore: api service to externalize endpoints

--- AspireSample.AppHost/Program.cs
+++ AspireSample.AppHost/Program.cs
@@ -2,7 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache);
 
 builder.AddProject<Projects.AspireSample_Web>("webfrontend")
     .WithExternalHttpEndpoints()
